6e143bb64d9cd868bf57229477a76650dff1538b
cha1ra <cha1ra.garnet@gmail.com>
2018-11-08 22:15:16 +0900

    ロゴイメージ作成

--- js/data.js
+++ js/data.js
@@ -3,7 +3,48 @@ Reserved Words Definition
 ref. https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Reserved_Words
 ----------------------------------*/
 
-
+const reservedWords = {
+    break: 0 ,
+    case: 0 ,
+    catch: 0 ,
+    class: 0 ,
+    continue: 0 ,
+    debugger: 0 ,
+    default: 0 ,
+    delete: 0 ,
+    do: 0 ,
+    else: 0 ,
+    enum: 0 ,
+    export: 0 ,
+    extends: 0 ,
+    finally: 0 ,
+    for: 0 ,
+    function: 0 ,
+    if: 0 ,
+    implements: 0 ,
+    import: 0 ,
+    in: 0 ,
+    instanceof: 0 ,
+    interface: 0 ,
+    let: 0 ,
+    new: 0 ,
+    package: 0 ,
+    private: 0 ,
+    protected: 0 ,
+    public: 0 ,
+    return: 0 ,
+    static: 0 ,
+    super: 0 ,
+    switch: 0 ,
+    this: 0 ,
+    throw: 0 ,
+    try: 0 ,
+    typeof: 0 ,
+    var: 0 ,
+    void: 0 ,
+    while: 0 ,
+    with: 0 
+}
 
 
 
